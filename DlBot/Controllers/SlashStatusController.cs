6921804faabb6ba41d9ceca666a4c43d75e09a5f
Lucas Falk <LRFalk01@gmail.com>
2017-04-30 14:01:47 -0400

    Add api for SlackStatus support

---
+++ DlBot/Controllers/SlashStatusController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using DlBot.Extensions;
+using DlBot.Models;
+using DlBot.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Serilog.Events;
+
+namespace DlBot.Controllers
+{
+    [Route("api/status")]
+    public class SlashStatusController : Controller
+    {
+        private readonly SlackService _slackService;
+
+        public SlashStatusController(SlackService slackService)
+        {
+            _slackService = slackService;
+        }
+
+        [HttpPost("{status}")]
+        public async Task<IActionResult> Post(string status)
+        {
+            status = status.ToLower();
+            
+            var inText = Request.Form["text"][0];
+            var inUserId = Request.Form["user_id"][0];
+            
+            if (status == "back")
+            {
+                _slackService.SetUserStatus(inUserId, "", "");
+                return Ok();
+            }
+
+            string emoji;
+            switch (status)
+            {
+                case "afk":
+                    emoji = ":afk:";
+                    break;
+                case "lunch":
+                    emoji = ":fork_and_knife:";
+                    break;
+                case "meeting":
+                    emoji = ":spiral_calendar_pad:";
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            if(string.IsNullOrWhiteSpace(inText))
+            {
+                var user = await _slackService.GetUserInfo(inUserId);
+                if (user.user.profile.status_emoji == emoji)
+                {
+                    _slackService.SetUserStatus(inUserId, "", "");
+                    return Ok();
+                }
+            }
+
+            inText = string.IsNullOrWhiteSpace(inText) ? DateTime.Now.ToString("h:mm tt") : $"{inText} - {DateTime.Now:h:mm tt}";
+            _slackService.SetUserStatus(inUserId, emoji, inText);
+
+            return Ok();
+        }
+    }
+}
--- DlBot/Models/SlackUserModel.cs
+++ DlBot/Models/SlackUserModel.cs
@@ -16,6 +16,20 @@ namespace DlBot.Models
         public string name { get; set; }
         public string real_name { get; set; }
         public string image_original { get; set; }
+        public SlackUserProfile profile { get; set; }
+    }
 
+    public class SlackUserProfile
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string status_text { get; set; }
+        public string status_emoji { get; set; }
+    }
+
+    public class SlackUserInfoModel
+    {
+        public bool ok { get; set; }
+        public SlackUserModel user { get; set; }
     }
 }
